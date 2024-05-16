bdf5c34753708cf02a6f92b26e02cd5e7ecc3b90
Pirateking001 <kieren.indrajith@gmail.com>
2024-05-17 00:06:45 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,12 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+            <article>
+                <h2>Post Title</h2>
+                <p>Lnnnnorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget ultrices purus. Ut varius, libero et efficitur rutrum, magna augue tincidunt mi, eget auctor sapien urna non justo.</p>
+                <p>Sed id dolor non quam varius malesuada. Nulla facilisi. Phasellus non libero sit amet magna facilisis eleifend.</p>
+                <p>Proin ut risus vel ligula volutpat efficitur. Sed sed semper dolor. Integer tincidunt, justo nec suscipit pulvinar, velit sem accumsan justo, sit amet elementum orci turpis non justo.</p>
+            </article>
       </main>
 
       <Footer />
